cbff37b7549fb9af1ecb1e8135ac6224191fba02
Andrew Wood <andrew.wood@gilwood-cs.co.uk>
2018-06-28 22:48:37 +0100

    update config

--- website/js/config.js
+++ website/js/config.js
@@ -1,11 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_IRw4g9Wrq', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '107sjdu70fqpki8mda20clu4ka', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: '	eu-west-1_CoUyJRVPX', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1i2r50gu4i7d92dg6kue0jsger', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://qjtia3a068.execute-api.eu-west-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://195paxsj18.execute-api.eu-west-1.amazonaws.com/Prod',
     },
     approval: {
         email: 'andrew@gilwood.org'
